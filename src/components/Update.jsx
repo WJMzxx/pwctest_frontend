03c6e091219bf9041f966b566a235624a297a5a3
zhenxxx <1367974185@qq.com>
2022-07-24 20:47:33 +0800

    v1.0.0

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -68,7 +68,7 @@ class Update extends Component {
 
     getTitle() {
         return (
-            <h3 className="text-center">Add u</h3>
+            <h3 className="text-center">update</h3>
         )
 
     }
